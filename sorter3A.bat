c37e2bb1877f3af49f727872388e19c05041624a
Equanimity <73202484+EquanimityX0880@users.noreply.github.com>
2022-06-11 02:46:41 -0500

    Update sorter3A.bat

--- sorter3A.bat
+++ sorter3A.bat
@@ -1,5 +1,7 @@
 @echo off
+:: variables
 set root=C:\Users\%USERNAME%\Desktop\root
 set target=C:\Users\%USERNAME%\Desktop\target
+:: For each subfolder in %root%, move each folder named "files" in %root% to %target%
 for /d /r %root% %%f in (files) do move "%%f" %target%
 pause
